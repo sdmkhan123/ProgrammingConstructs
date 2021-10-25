bcfd4781b7c4ba1716e45d481b4dcd3517e1bc15
sdmkhan123 <sdmkhan126410@gmail.com>
2021-10-26 02:19:26 +0530

    Find min and max out 5 num of 3 digit

--- IfAndElse.js
+++ IfAndElse.js
@@ -1,1 +1,28 @@
+console.log("************************************************************************************");
 console.log("Welcome to Selection Practice Problems with if & else");
+console.log("************************************************************************************");
+console.log("===============================================================================");
+console.log("Welcome to Selection Practice Problems with if & else");
+console.log("===============================================================================");
+//1.Write a program that reads 5 Random 3 Digit values and then outputs the minimum and the maximum value
+numbers=[];
+for(i=0;i<5;i++)
+{
+ numbers[i]=Math.floor((Math.random() * 900) + 100);
+}
+let max=0;
+let min=1000;
+for(i=0;i<5;i++)
+{
+    if(max<numbers[i])
+    {
+        max=numbers[i];
+    }
+    if(min>numbers[i])
+    {
+        min=numbers[i];
+    }
+}
+console.log("Max no is "+max);
+console.log("Min no is "+min);
+console.log("===============================================================================");
