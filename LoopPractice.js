3368e7b73aa4584945647c95b1f5896c65912183
sdmkhan123 <sdmkhan126410@gmail.com>
2021-10-28 20:48:51 +0530

    Check a Number is Prime or not

--- LoopPractice.js
+++ LoopPractice.js
@@ -9,4 +9,21 @@ for(let i = 1; i <= input; i++)
     result = result*2;
     console.log("2 to the power " + i + " is " + result);
 }
+//UC-2
+let num = 9;
+let is_prime = true;
+if (num == 1)
+{
+    console.log("1 is neither prime nor composite");
+    process.exit(0);
+}
+for (let i = 2; i <= Math.sqrt(num); i++)
+{
+    if (num % i == 0)
+        is_prime = false;
+}
+if (is_prime)
+    console.log(num + " is a Prime number");
+else
+    console.log(num + " is not Prime number");
 console.log("===============================================================================");
