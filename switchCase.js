64f5a1eba4bc1eb0ae73831b1008ab42cddbfddf
sdmkhan123 <sdmkhan126410@gmail.com>
2021-10-28 20:32:30 +0530

    Using Switch Case Convert number into UnitPlace

--- switchCase.js
+++ switchCase.js
@@ -65,4 +65,33 @@ switch(day){
     default:
         console.log("Enter valid number for Weekday");
 }
+//UC-4
+let num = 100;
+switch(num)
+{
+    case 1:
+        console.log("Units");
+        break;
+    case 10:
+        console.log("Tens");
+        break;
+    case 100:
+        console.log("Hundreds");
+        break;
+    case 1000:
+        console.log("Thousand");
+        break;
+    case 10000:
+        console.log("Tens of Thousands");
+        break;
+    case 100000:
+        console.log("Hundreds of Thousands");
+        break;
+    case 1000000:
+        console.log("Millions ");
+        break;
+    default:
+        console.log("enter correct number");
+        break;
+}
 console.log("===============================================================================");
