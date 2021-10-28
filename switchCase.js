2532c8fb812c8b2cff31c0e08c7cc543ba16f85d
sdmkhan123 <sdmkhan126410@gmail.com>
2021-10-28 20:21:15 +0530

    Using Switch Case Convert number into words

---
+++ switchCase.js
@@ -0,0 +1,40 @@
+console.log("************************************************************************************");
+console.log("Welcome to Selection Practice Problems with Switch case");
+console.log("************************************************************************************");
+console.log("===============================================================================");
+let number = Math.floor((Math.random()*10));
+switch(number){
+    case 0:
+        console.log("Zero");
+        break;    
+    case 1:
+        console.log("One");
+        break;
+    case 2:
+        console.log("Two");
+        break;           
+    case 3:
+        console.log("Three");
+        break;
+    case 4:
+        console.log("Four");
+        break;
+    case 5:
+        console.log("Five");
+        break;
+    case 6:
+        console.log("Six");
+        break;
+    case 7:
+        console.log("Seven")
+        break;
+    case 8:
+        console.log("Eight")
+        break;
+    case 9:
+        console.log("Nine")
+        break;                                       
+    default:
+        console.log("Enter valid number");
+}
+console.log("===============================================================================");
