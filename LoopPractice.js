d69ec43c5c1b4bba6d609f6fdd4c4e94b26213ad
sdmkhan123 <sdmkhan126410@gmail.com>
2021-10-28 20:44:01 +0530

    Find Power of Two

---
+++ LoopPractice.js
@@ -0,0 +1,12 @@
+console.log("************************************************************************************");
+console.log("Welcome to Selection Practice Problems with Loops stmt");
+console.log("************************************************************************************");
+//UC-1
+let input = 5;
+let result = 1;
+for(let i = 1; i <= input; i++)
+{
+    result = result*2;
+    console.log("2 to the power " + i + " is " + result);
+}
+console.log("===============================================================================");
