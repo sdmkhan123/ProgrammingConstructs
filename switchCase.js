a6422997a70dac91a5b91895ab424f1759e11f03
sdmkhan123 <sdmkhan126410@gmail.com>
2021-10-28 20:27:57 +0530

    Using Switch Case Convert number into WeekDays

--- switchCase.js
+++ switchCase.js
@@ -2,6 +2,7 @@ console.log("*******************************************************************
 console.log("Welcome to Selection Practice Problems with Switch case");
 console.log("************************************************************************************");
 console.log("===============================================================================");
+//UC-1
 let number = Math.floor((Math.random()*10));
 switch(number){
     case 0:
@@ -36,5 +37,32 @@ switch(number){
         break;                                       
     default:
         console.log("Enter valid number");
+}
+//UC-2
+let day = Math.floor((Math.random()*7));
+switch(day){
+    case 0:
+        console.log("Sunday");
+        break;   
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;            
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    default:
+        console.log("Enter valid number for Weekday");
 }
 console.log("===============================================================================");
