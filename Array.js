78a09ade197e37f74d27f1d0bd023cb420cf6cb7
sdmkhan123 <sdmkhan126410@gmail.com>
2021-10-28 21:52:45 +0530

    Sum is Three Number is Zero

--- Array.js
+++ Array.js
@@ -27,3 +27,22 @@ let sortArray = array.sort();
 console.log(...array);
 console.log("Second Largest number in sorted array  : " + sortArray[sortArray.length-2]);
 console.log("Second Smallest number in sorted array : " + sortArray[1]);
+//UC-3
+let arr = [];
+let num1 = 1;
+arr.push(num1);
+let num2 = 2;
+arr.push(num2);
+let num3 = -3;
+arr.push(num3);
+let sum = 0;
+function Sum(num) 
+{
+    sum = sum+num;
+}
+arr.forEach(Sum);
+if (sum == 0)
+    console.log('Sum is Zero');
+else{
+    console.log('Sum is not zero.');
+}
